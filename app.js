1730751e26328415dcf617c051963bb81623bd49
RauboLuk <rauboluk@gmail.com>
2021-03-11 22:01:15 +0100

    added .env and mongoose

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
+require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
-const fileUpload = require('express-fileupload');
+const fileUpload = require('express-fileupload')
 
 const app = express()
 // Heroku dynamically sets a port
