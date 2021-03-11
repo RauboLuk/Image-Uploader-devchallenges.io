9effdbdf894e9dacba79551f1b96dc2f92a3a1de
RauboLuk <rauboluk@gmail.com>
2021-03-11 23:52:40 +0100

    some binary data uploads to mongo

--- app.js
+++ app.js
@@ -1,50 +1,72 @@
 const path = require('path');
 const dotenv = require('dotenv')
 const express = require('express')
+const mongoose = require('mongoose')
 const cors = require('cors')
 const fileUpload = require('express-fileupload')
 
 const app = express()
+dotenv.config({ path: path.resolve(process.cwd(), '.env.local') })
 
-const envConfig = dotenv.config({ path: path.resolve(process.cwd(), '.env.local') })
-const PORT = process.env.PORT || envConfig.PORT
+const PORT = process.env.PORT
+
+const imgModel = require('./models')
+
+mongoose.connect(process.env.MONGO_URL,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }, () => {
+    console.log('connected')
+});
 
 app.use(fileUpload({
   createParentPath: true
 }));
 
+app.use(express.json()) // for parsing application/json
 app.use(express.static('dist'))
 app.use(cors())
 
-app.post('/upload', function(req, res) {
+app.post('/upload', async (req, res) => {
   try {
     if(!req.files) {
-        res.send({
-            status: false,
-            message: 'No file uploaded'
-        });
+      res.send({
+          status: false,
+          message: 'No file uploaded'
+      });
     } else {
-        //Use the name of the input field (i.e. "avatar") to retrieve the uploaded file
-        let img = req.files.img;
-        
-        //Use the mv() method to place the file in upload directory (i.e. "uploads")
-        img.mv('./uploads/' + img.name);
+      //Use the name of the input field (i.e. "avatar") to retrieve the uploaded file
+      let img = req.files.img
+      
+      
+      //Use the mv() method to place the file in upload directory (i.e. "uploads")
+      img.mv('./uploads/' + img.name)
+      
+      const newImg = new imgModel({
+        img: {
+          data: img.data,
+          contentType: 'image/jpg'
+        }
+      })
 
-        //send response
-        res.send({
-            status: true,
-            message: 'File is uploaded',
-            data: {
-                name: img.name,
-                mimetype: img.mimetype,
-                size: img.size
-            }
-        });
+      await newImg.save()
+
+      //send response
+      // res.send({
+      //     status: true,
+      //     message: 'File is uploaded',
+      //     data: {
+      //         name: img.name,
+      //         mimetype: img.mimetype,
+      //         size: img.size
+      //     }
+      // })
     }
   } catch (err) {
       res.status(500).send(err);
   }
-});
+})
 
 app.get('/health', (req, res) => {
   res.send('ok')
